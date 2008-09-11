8b1489afa1ae2ffa8b3b6e0f3c5f53e795f0f9e9
taahwrux <taahwrux@klap.(none)>
2008-09-11 14:19:03 -0400

    wrapping fetch.py into class DataAdaptor

--- fetch.py
+++ fetch.py
@@ -2,12 +2,53 @@
 
 import urllib
 
-add = "http://finance.google.com/finance/historical?q=NYSE:IBM&output=csv"
+# "http://finance.google.com/finance/historical?q=NYSE:IBM&output=csv"
+class DataAdaptor(object):
+    def __init__(self):
+     	self.__mURL = ""
+	self.__mData = None
+        self.__mDict = dict()
 
-f = urllib.urlopen(add)
-csv = f.read()
+    def getURL(self):
+    	return self.__mURL
 
-print csv
+    def setURL(self, url):
+    	self.__mURL = url
 
-f.close()
+    url = property(getURL, setURL)
 
+    def InsertItem(self, key, value):
+        self.__mDict[key] = value
+    
+
+
+    def QueryItem(self, key):
+    	return self.__mDict[key]
+
+    def Generate(self):
+    	pa = self.__mDict
+	all = []
+	for x in pa:
+	   all.append("%s=%s" % (x, pa[x]))
+ 	
+	return self.url + "&".join(all)
+    def Fetch(self):
+       	s = self.Generate()
+	f = urllib.urlopen(s)
+	data = f.read()
+	f.close()
+	print data
+    
+    
+
+        
+        
+    
+    
+if __name__ == "__main__":
+    gFin = DataAdaptor()
+    gFin.url = "http://finance.google.com/finance/historical?"
+    gFin.InsertItem('q', 'NYSE:IBM')
+    gFin.InsertItem('output', 'csv')
+
+    print gFin.Fetch()
