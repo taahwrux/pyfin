bafef894ca6e0c3a34a7018bb4ec2134ee2a364b
Kenneth Feng <kennethzfeng@gmail.com>
2008-09-13 16:34:23 -0400

    container.py: complete all the getter and setter

--- container.py
+++ container.py
@@ -13,8 +13,44 @@ class DataContainer(object):
     def getDate(self):
         return self.__mDate
     def setDate(self, date):
-        self.__mDate = date
+        if isinstance(date, datetime.date):
+            self.__mDate = date
     date = property(getDate, setDate)
+
+    # Open
+    def getOpen(self):
+        return self.__mOpen
+    def setOpen(self, open):
+        self.__mOpen = open
+    open = property(getOpen, setOpen)
+
+    # High
+    def getHigh(self):
+        return self.__mHigh
+    def setHigh(self, high):
+        self.__mHigh = high
+    high = property(getHigh, setHigh)
+
+    # Low
+    def getLow(self):
+        return self.__mLow
+    def setLow(self, low):
+        self.__mLow = low
+    low = property(getLow, setLow)
+
+    # Close
+    def getClose(self):
+        return self.__mClose
+    def setClose(self, close):
+        self.__mClose = close
+    close = property(getClose, setClose)
+
+    # Volume
+    def getVolume(self):
+        return self.__mVolume
+    def setVolume(self, volume):
+        self.__mVolume = volume
+    volume = property(getVolume, setVolume)
 
 
 if __name__ == "__main__":
